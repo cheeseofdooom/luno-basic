2fe2ccb9f36be75e1f132d722fb99cae87a71870
cheeseofdooom <diren123@gmail.com>
2022-08-07 22:52:39 +0200

    iam keyless poilcy

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "wordpress" {
   associate_public_ip_address = true
   subnet_id                   = sort(data.aws_subnet_ids.subnets.ids)[0]
   security_groups             = [aws_security_group.ec2_secgrp.id]
-  iam_instance_profile        = "aws-cli-user"
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.id
   user_data                   = data.template_file.userdata.rendered
 
   tags = merge(local.tags, {
@@ -14,14 +14,16 @@ resource "aws_instance" "wordpress" {
 
 resource "aws_db_instance" "wordpressdb" {
   allocated_storage    = 10
-  engine               = "mariadb"
-  engine_version       = "10.6"
+  engine               = "mysql"
+  engine_version       = "5.7"
   instance_class       = var.rds_instance_type
   name                 = aws_ssm_parameter.dbname.value
   username             = aws_ssm_parameter.dbuser.value
   password             = aws_ssm_parameter.dbpassword.value
-  #parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql5.7"
+  db_subnet_group_name = aws_db_subnet_group.dbsubnet.id
   skip_final_snapshot  = true
+  
 }
 
 resource "aws_ssm_parameter" "dbname" {
@@ -47,6 +49,50 @@ resource "random_password" "password" {
   special          = true
   override_special = "_%@"
 }
+
+resource "aws_db_subnet_group" "dbsubnet" {
+    name = "wordpress-subnet"
+    description = "word press subnet group"
+    subnet_ids = data.aws_subnet_ids.subnets.ids
+    tags = local.tags
+  
+}
+
+
+resource "aws_iam_role" "ec2role" {
+    name  = "ec2forssm"
+
+    assume_role_policy = <<EOF
+   {
+     "Version": "2012-10-17"
+     "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+
+                },
+                "Effect": "Allow",
+                "Sid": ""
+
+            }
+        ]
+}
+EOF
+  
+}
+
+resource "aws_iam_role_policy_attachment" "ec2policy" {
+    role       = aws_iam_role.ec2role.name
+    policy_arn = "arn:aws:iam:aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+    name = "ec2_profile"
+    role = aws_iam_role.ec2role.name
+  
+}
+  
 
 
 
